8c229884ab466bf563e1a66a79bcda41afefb926
TheSteff5 <43687485+TheSteff5@users.noreply.github.com>
2023-03-14 09:09:05 +0100

    code cleanup

--- src/at/fhhgb/mtd/gop/veccy/Veccy.java
+++ src/at/fhhgb/mtd/gop/veccy/Veccy.java
@@ -19,33 +19,17 @@ public class Veccy extends Application {
         double y;
         Rectangle[] sinus1 = new Rectangle[2000];
         Rectangle[] sinus2 = new Rectangle[2000];
-        Line[] lines = new Line[2000];
         for(int x=0; x < 2000; x++){
             y =  stage.getHeight()/4 + 100 * Math.sin((x * (Math.PI/180)) - Math.PI);
             sinus1[x] = new Rectangle(new Point(x,(int)y),2,2);
             model.addShape(sinus1[x]);
-        }
-
-        for(int x=0; x < 2000; x++){
             y = (stage.getHeight() - stage.getHeight()/4) + 100 * Math.sin((x * (Math.PI/180)) );
             sinus2[x] = new Rectangle(new Point(x,(int)y),2,2);
             model.addShape(sinus2[x]);
         }
 
-
-        for(int x=0; x < lines.length; x+=4){
-
-
-            lines[x] = new Line(sinus1[x].getPoint().getX(), sinus1[x].getPoint().getY(), sinus2[x].getPoint().getX(), sinus2[x].getPoint().getY());
-            model.addShape(lines[x]);
+        for(int x=0; x < 2000; x+=4){
+            model.addShape( new Line(sinus1[x].getPoint().getX(), sinus1[x].getPoint().getY(), sinus2[x].getPoint().getX(), sinus2[x].getPoint().getY()));
         }
-
-
-
-        //Rectangle r1 = new Rectangle(new Point(1,1),10,10);
-
-
-
-
     }
 }
