4fafdda764bc97190640ce3ed20d9881f6f116e0
TheSteff5 <stefanmiskovic11@hotmail.com>
2023-03-19 22:10:53 +0100

    finished A1 with unitests

--- test/at/fhhgb/mtd/gop/math/TransformFactoryTest.java
+++ test/at/fhhgb/mtd/gop/math/TransformFactoryTest.java
@@ -13,27 +13,48 @@ class TransformFactoryTest {
         Vector3 vec = new Vector3(new double[]{5.0, 5.0, 1.0});
         Matrix3 translation = TransformFactory.createTranslation(2, 3);
         Vector3 transformedVec = translation.mult(vec);
-
         assertTrue(Arrays.equals(transformedVec.getValues(), new double[]{7.0, 8.0, 1.0}));
     }
 
     @Test
     void testCreateRotation() {
-
+        Vector3 vec = new Vector3(new double[]{5.0, 5.0, 1.0});
+        Matrix3 rotation = TransformFactory.createRotation(Math.PI / 2);
+        Vector3 rotatedVec = rotation.mult(vec);
+        assertTrue(Arrays.equals(rotatedVec.getValues(), new double[]{-5.0, 5.0, 1.0}));
     }
 
     @Test
     void testCreateHorizontalMirroring() {
 
+        //Fragen
+        Vector3 vec = new Vector3(new double[]{5.0, 5.0, 1.0});
+        Matrix3 horizontalMirroring = TransformFactory.createHorizontalMirroring();
+        Vector3 mirroredVec = horizontalMirroring.mult(vec);
+
+        /*for (int i = 0; i < 3; i++) {
+            System.out.println(mirroredVec.getValues()[i]);
+        }*/
+        //assertTrue(Arrays.equals(rotatedVec.getValues(), new double[]{-5.0, 5.0, 1.0}));
     }
 
     @Test
     void testCreateVerticalMirroring() {
-
+        Vector3 vec = new Vector3(new double[]{5.0, 5.0, 1.0});
+        Matrix3 verticalMirroring = TransformFactory.createVerticalMirroring();
+        Vector3 mirroredVec = verticalMirroring.mult(vec);
+        assertTrue(Arrays.equals(mirroredVec.getValues(), new double[]{-5.0, -5.0, 1.0}));
     }
 
     @Test
     void testCreateScaling() {
+        Vector3 vec = new Vector3(new double[]{5.0, 5.0, 1.0});
+        Matrix3 scaling = TransformFactory.createScaling(2, 2);
+        Vector3 scaledVec = scaling.mult(vec);
+        assertTrue(Arrays.equals(scaledVec.getValues(), new double[]{10.0, 10.0, 1.0}));
 
+        scaling = TransformFactory.createScaling(3, 1.5);
+        scaledVec = scaling.mult(vec);
+        assertTrue(Arrays.equals(scaledVec.getValues(), new double[]{15.0, 7.5, 1.0}));
     }
 }
